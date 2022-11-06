0508b7bd094818a5c5bd6a22d6cb74beb2c7b848
Shifan <shifanchippushifan@gmail.com>
2022-11-06 11:45:46 +0530

    balloon random generated

--- interface.py
+++ interface.py
@@ -4,6 +4,12 @@ from sys import exit
 pygame.init()
 screen=pygame.display.set_mode((1400,700))
 
+class balloon:
+    def __init__(self,color):
+        self.color=color
+        self.xpos=random.randrange(0,700,100)
+        self.ypos=random.randint(0,700)
+
 def set_screen():
     pygame.display.set_caption("computer vision game")
     clock=pygame.time.Clock()
@@ -11,16 +17,16 @@ def set_screen():
     test_surface=pygame.Surface((1400,700))
     test_surface.fill('White')
     text_surface=test_font.render("mygame",False,'Red')
-
     ball=pygame.image.load("ball.png").convert_alpha()
-    colorflag=1
-    red=pygame.image.load("red.png").convert_alpha()
-    blue=pygame.image.load("blue.png").convert_alpha()
-    blue=pygame.transform.scale(blue,(500,500))
-    xposr=random.randrange(0,700,35)
-    xposb=random.randrange(0,700,150)
-    y_posr=0
-    y_posb=0
+    r=pygame.image.load("red.png").convert_alpha()
+    red=balloon(r)
+    b=pygame.image.load("blue.png").convert_alpha()
+    b=pygame.transform.scale(b,(500,500))
+    blue=balloon(b)
+    xposr=red.xpos
+    xposb=blue.xpos
+    y_posr=red.ypos
+    y_posb=blue.ypos
     while True:
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
@@ -31,26 +37,28 @@ def set_screen():
         if y_posr > -700 and y_posb > -700:
             y_posb=y_posb-4
             y_posr=y_posr-4
-            screen.blit(red,(xposr,y_posr))
-            screen.blit(blue,(xposb,y_posb))
+            print("1")
+            screen.blit(r,(xposr,y_posr))
+            screen.blit(b,(xposb,y_posb))
+
         else:
             if y_posr<=-700:
-                y_posr=700
+                red=balloon(r)
+                x_posr=red.xpos
+                y_posr=red.ypos
                 y_posb=y_posb-4
-                screen.blit(red,(xposr,y_posr))
-                screen.blit(blue,(xposb,y_posb))
+                screen.blit(r,(xposr,y_posr))
+                screen.blit(b,(xposb,y_posb))
             elif y_posb<=-700:
-                y_posb=700
+                blue=balloon(b)
+                xposb=blue.xpos
+                y_posb=blue.ypos
                 y_posr=y_posr-4
-                screen.blit(blue,(xposb,y_posb))
-                screen.blit(red,(xposr,y_posr))
+                screen.blit(b,(xposb,y_posb))
+                screen.blit(r,(xposr,y_posr))
 
-            xposb=random.randint(0,700)
-            xposr=random.randint(0,700)
-            if colorflag==1:
-                colorflag=0
-            else:
-                colorflag=1
+            # xposb=random.randint(0,700)
+            # xposr=random.randint(0,700)
 
         pygame.display.update()
         clock.tick(30)
